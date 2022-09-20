d0994dcc97df86df8b082c736dc458c184bb4eab
Jaron van Well <jaronvwell@gmail.com>
2022-09-20 21:08:59 +0200

    code seems to work

--- src/Main.java
+++ src/Main.java
@@ -8,13 +8,13 @@ public class Main {
 
         for (int y = 0; y < arrSizeY ; y++) {
             for (int x = 0; x <arrSizeX ; x++) {
-                arr[y][x] = new Random().nextInt(5-1) + 1;
+                arr[y][x] = new Random().nextInt(10000-1) + 1;
                 System.out.print(arr[y][x] + ", ");
             }
             System.out.println("");
         }
 
-        System.out.println(Main.<Integer>biggestArraySum(arr));
+        System.out.println(Main.<Integer>biggestArraySum2(arr));
 
 
 
@@ -25,20 +25,32 @@ public class Main {
         for (T[] l: arr) {
             int sum = 0;
             for (int i = 0; i < l.length; i++) {
-                sum += (int)l[0];
+                sum += (int)l[i];
             }
             if(selectedArrSum < sum) selectedArrSum = sum;
         }
         return selectedArrSum;
     }
     public static <T extends Number> int biggestArraySum2(T[][] arr){
-        long[] sums = new long[arr.length];
+        int[] sums = new int[arr.length];
         for (int y = 0; y < arr.length ; y++) {
-            long sum = 0;
+            int sum = 0;
             for (int x = 0; x < arr[y].length ; x++) {
-                sum += arr[y][x];
+                sum += (int) arr[y][x];
+
             }
-            System.out.println("");
+            sums[y] = sum;
+            System.out.println("- " + sum);
         }
+        int selectedArrSum = 0;
+        int highestIndex = -1;
+        for (int i = 0;i< sums.length; i++) {
+            if(selectedArrSum < sums[i]) {
+                selectedArrSum = sums[i];
+                highestIndex =i;
+            }
+        }
+        return highestIndex;
+
     }
 }
